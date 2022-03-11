cdbb5d646f29036bf7e9b430bdcc178f5e2e0e0d
bkalmanj <58172525+bkalmanj@users.noreply.github.com>
2022-03-11 12:10:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "ubuntuJ"
+  default     =  "coeAwsKeyPair"
 }
 
 variable "instance_type" {
